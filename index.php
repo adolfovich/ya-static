c3498d8fa91285d26ec8a8ad39b61a48e75aa711
root <root>
2019-11-20 17:22:30 +0300

    disable display errors

--- index.php
+++ index.php
@@ -14,11 +14,7 @@
 
 session_start();
 
-//error_reporting(0);
-
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+error_reporting(0);
 
 // Support Database
 include ('_conf.php');
